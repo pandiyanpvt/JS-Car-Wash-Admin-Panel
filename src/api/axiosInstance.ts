f99bd9f248c779d452e7a8464bf265d7d78fd171
Kuganesan Bavithragithan <skbavi61@gmail.com>
2025-12-31 14:33:57 +0530

    modified

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://js-car-wash-backend-production.up.railway.app/api'
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://js-car-wash-backend-production.up.railway.app/api'
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api'
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
